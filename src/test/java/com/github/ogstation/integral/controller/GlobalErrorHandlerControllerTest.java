a9bfdb1acb5eca157a1f32119eb9a4c31ea8e87b
Hao Wen <wenhao@126.com>
2014-07-19 11:44:47 +0800

    [INTEGRAL][Wen Hao] - init commit.

---
+++ src/test/java/com/github/ogstation/integral/controller/GlobalErrorHandlerControllerTest.java
@@ -0,0 +1,98 @@
+package com.github.ogstation.integral.controller;
+
+import com.github.ogstation.integral.config.MessageResolver;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+public class GlobalErrorHandlerControllerTest
+{
+
+    private MockMvc mockMvc;
+
+    @Mock
+    private MessageResolver messageResolver;
+
+    @InjectMocks
+    private GlobalErrorHandlerController globalErrorHandlerController;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        MockitoAnnotations.initMocks(this);
+        this.mockMvc = standaloneSetup(globalErrorHandlerController).build();
+    }
+
+    @Test
+    public void should_be_able_to_handle_400_error() throws Exception
+    {
+        // then
+        this.mockMvc.perform(post("/api/error/400"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status", is("400")));
+    }
+
+    @Test
+    public void should_be_able_to_handle_401_error() throws Exception
+    {
+        // then
+        this.mockMvc.perform(post("/api/error/401"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status", is("401")));
+    }
+
+    @Test
+    public void should_be_able_to_handle_403_error() throws Exception
+    {
+        // then
+        this.mockMvc.perform(post("/api/error/403"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status", is("403")));
+    }
+
+    @Test
+    public void should_be_able_to_handle_404_error() throws Exception
+    {
+        // then
+        this.mockMvc.perform(post("/api/error/404"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status", is("404")));
+    }
+
+    @Test
+    public void should_be_able_to_handle_405_error() throws Exception
+    {
+        // then
+        this.mockMvc.perform(post("/api/error/405"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status", is("405")));
+    }
+
+    @Test
+    public void should_be_able_to_handle_415_error() throws Exception
+    {
+        // then
+        this.mockMvc.perform(post("/api/error/415"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status", is("415")));
+    }
+
+    @Test
+    public void should_be_able_to_handle_500_error() throws Exception
+    {
+        // then
+        this.mockMvc.perform(get("/api/error/500"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status", is("500")));
+    }
+}
